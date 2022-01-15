1759b117b31426bb847f81e5f489d02a3166e800
aliHaider-911 <84412171+aliHaider-911@users.noreply.github.com>
2022-01-15 23:50:28 +0500

    new commit

---
+++ custom.js
@@ -0,0 +1,23 @@
+
+// const laptops = 100;
+// console.log(laptops);
+
+
+
+// let remainingLaptops = laptops - 30;
+
+
+// console.log(laptops);
+// console.log(remainingLaptops);
+
+
+
+let totalScore = 199;
+
+// totalScore++;
+// console.log("asjda")
+
+document.write(totalScore)
+
+
+	
